94e98236d928a0af971cbf3e07d685aaf014e8d4
OW-Marns <marns0501@gmail.com>
2025-10-24 14:51:38 +0800

    Export interface for Checklist

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import cn from "classnames";
 
-interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
+export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export const Card: React.FC<CardProps> = (props) => {
   return (
