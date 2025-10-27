f13da97973df01a59274b8c2c3cae021a7800ef3
OW-Marns <marns0501@gmail.com>
2025-10-27 09:39:04 +0800

    Update textarea

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -6,34 +6,54 @@ interface TextAreaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement>,
     Pick<ChecklistItem, "helperText"> {
   label?: string;
+  parentClassname?: string;
+  labelClassname?: string;
+  inputClassname?: string;
+  helperClassname?: string;
 }
 
-export const TextArea: React.FC<TextAreaProps> = ({
-  className,
-  label,
-  helperText,
-  rows = 2,
-  ...restProps
-}) => {
+export const TextArea: React.FC<TextAreaProps> = (props) => {
+  const {
+    parentClassname,
+    labelClassname,
+    inputClassname,
+    helperClassname,
+    id = "textarea",
+    label,
+    helperText,
+    rows = 2,
+    ...restProps
+  } = props;
+
   return (
-    <div className="w-full flex flex-col gap-1">
+    <div className={cn("w-full flex flex-col gap-1", parentClassname)}>
       {label && (
-        <label className="block text-sm font-medium text-gray-600">
-          Notes:
+        <label
+          htmlFor={id}
+          className={cn(
+            "block text-sm font-medium text-gray-600",
+            labelClassname
+          )}
+        >
+          {label}
         </label>
       )}
 
       <textarea
         {...restProps}
+        id={id}
+        name={id}
         rows={rows}
         className={cn(
           "outline-none w-full p-3 border border-gray-300 rounded-md focus:ring-1 focus:ring-sky-500 focus:border-sky-500 text-sm",
-          className
+          inputClassname
         )}
       />
 
       {helperText && (
-        <span className="text-xs text-gray-500">{helperText}</span>
+        <span className={cn("text-xs text-gray-500", helperClassname)}>
+          {helperText}
+        </span>
       )}
     </div>
   );
